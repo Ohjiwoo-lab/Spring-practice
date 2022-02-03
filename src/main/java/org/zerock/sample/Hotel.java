2a256f6b9f7c45f76a2164635e4c890c91bf7be5
OhJiwoo <jiu3159@naver.com>
2022-02-03 11:05:33 +0900

    test: dependency injection test

---
+++ src/main/java/org/zerock/sample/Hotel.java
@@ -0,0 +1,24 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class Hotel {
+
+	@NonNull
+	private Chef chef;
+
+	private String name;
+	
+	//public Hotel(Chef chef) {
+	//	this.chef = chef;
+	//}
+}
